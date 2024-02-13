c923b689b34f3aa14cf505678c652cf9efb5a093
Fábio Maiorano <fsmaiorano@gmail.com>
2024-02-13 12:37:03 +0000

    implement getTicketsBySectorId

---
+++ src/Application/UseCases/Ticket/Queries/GetTicketsBySectorIdQuery.cs
@@ -0,0 +1,62 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Application.UseCases.Ticket.Queries;
+
+public record GetTicketsBySectorIdQuery : IRequest<GetTicketsBySectorIdResponse>
+{
+    public Guid SectorId { get; set; }
+}
+
+public class GetTicketsBySectorIdResponse : BaseResponse
+{
+    public List<TicketDto>? Tickets { get; set; }
+}
+
+public class GetTicketBySectorHandler(ILogger<GetTicketByIdHandler> logger, IDataContext context) : IRequestHandler<GetTicketsBySectorIdQuery, GetTicketsBySectorIdResponse>
+{
+    private readonly IDataContext _context = context;
+    private readonly ILogger<GetTicketByIdHandler> _logger = logger;
+
+    public async Task<GetTicketsBySectorIdResponse> Handle(GetTicketsBySectorIdQuery request, CancellationToken cancellationToken)
+    {
+        var response = new GetTicketsBySectorIdResponse();
+
+        try
+        {
+            _logger.LogInformation("GetTicketsBySectorId: {@Request}", request);
+
+            var tickets = await _context.Tickets.Where(x => x.SectorId == request.SectorId).ToListAsync(cancellationToken);
+
+            if (tickets is null)
+            {
+                _logger.LogWarning("GetTicketsBySectorId: Tickets not");
+                response.Message = "Tickets not found";
+
+                return response;
+            }
+
+            response.Success = true;
+            response.Message = "Tickets found";
+            response.Tickets = tickets.Select(x => new TicketDto
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Content = x.Content,
+                UserId = x.UserId,
+                HolderId = x.HolderId,
+                SectorId = x.SectorId
+            }).ToList();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "GetTicketsBySectorId: {@Request}", request);
+            response.Message = ex.Message;
+        }
+
+        return response;
+    }
+}
--- tests/IntegrationTest/Application/UseCases/Ticket/Queries/GetTicketQueriesIntegrationTest.cs
+++ tests/IntegrationTest/Application/UseCases/Ticket/Queries/GetTicketQueriesIntegrationTest.cs
@@ -12,7 +12,7 @@ public class GetTicketQueriesIntegrationTest : Testing
     }
 
     [TestMethod]
-    public async Task GetTicketsBySectorIdQuery()
+    public async Task GetTicketsByIdQuery()
     {
         var createTicketIntegrationTest = new CreateTicketIntegrationTest();
         _ = createTicketIntegrationTest.CreateTicket();
@@ -20,6 +20,22 @@ public class GetTicketQueriesIntegrationTest : Testing
         var query = new GetTicketByIdQuery
         {
             Id = CreateTicketIntegrationTest.CreatedTicket!.Id,
+        };
+
+        var getTicketByIdResponse = await SendAsync(query);
+        Assert.IsNotNull(getTicketByIdResponse);
+        Assert.IsTrue(getTicketByIdResponse.Success);
+    }
+
+    [TestMethod]
+    public async Task GetTicketsBySectorIdQuery()
+    {
+        var createTicketIntegrationTest = new CreateTicketIntegrationTest();
+        _ = createTicketIntegrationTest.CreateTicket();
+
+        var query = new GetTicketsBySectorIdQuery
+        {
+            SectorId = CreateTicketIntegrationTest.CreatedTicket!.SectorId,
         };
 
         var getTicketByIdResponse = await SendAsync(query);
