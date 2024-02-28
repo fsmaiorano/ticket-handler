f097f23f3d2ef733535eef80540c8241b7f8bb4d
fsmaiorano <fabio.maiorano@armis.pt>
2024-02-28 10:55:58 +0000

    implement AssignUserToSectorValidatorValidator

---
+++ src/Application/UseCases/User/Commands/AssignUserToSector/AssignUserToSectorValidator.cs
@@ -0,0 +1,17 @@
+﻿using Application.UseCases.User.Commands.AssignUserToSector;
+using FluentValidation;
+
+namespace Application.UseCases.User.Commands.AssignUserToSectorValidator;
+public class AssignUserToSectorValidatorValidator : AbstractValidator<AssignUserToSectorCommand>
+{
+    public AssignUserToSectorValidatorValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(x => x.SectorsId)
+            .NotEmpty()
+            .NotNull();
+    }
+}
