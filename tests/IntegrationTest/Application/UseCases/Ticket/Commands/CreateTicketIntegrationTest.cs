e4b73aa8f6d9f5fd5f00f7ef4121efa2af7fde46
Fábio Maiorano <fsmaiorano@gmail.com>
2024-02-29 21:58:32 +0000

    fix ticket integration test error

--- tests/IntegrationTest/Application/UseCases/Ticket/Commands/CreateTicketIntegrationTest.cs
+++ tests/IntegrationTest/Application/UseCases/Ticket/Commands/CreateTicketIntegrationTest.cs
@@ -55,6 +55,7 @@ public class CreateTicketIntegrationTest : Testing
             UserId = CreateUserIntegrationTest.CreatedUser!.Id,
             HolderId = CreateHolderIntegrationTest.CreatedHolder!.Id,
             SectorId = CreateSectorIntegrationTest.CreatedSector!.Id,
+            AssigneeId = CreateUserIntegrationTest.CreatedUser!.Id
         };
 
         return command;
